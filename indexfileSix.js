b17a1c4c9c4c7ffb155eba26a7616c2261c639d0
IrynkaZozuliak <irynka.zozuliak@gmail.com>
2023-04-18 08:44:23 +0000

    gdfv

---
+++ indexfileSix.js
@@ -0,0 +1,34 @@
+//Завдання 6
+let firstName = "Iryna";
+let secondName = "Zozuliak";
+let educationGroup = 106;
+let myAge = 22;
+
+
+let familyStatus = true;
+
+console.log(`educationGroup: ${educationGroup}, type: ${typeof (educationGroup)}`);
+console.log(`myAge: ${myAge}, type: ${typeof (myAge)}`);
+console.log(`familyStatus: ${familyStatus}, type: ${typeof (familyStatus)}`);
+console.log(`firstName: ${firstName}, type: ${typeof (firstName)}`);
+console.log(`secondName: ${secondName}, type: ${typeof (secondName)}`);
+
+let myFirstEducation ;
+console.log(`myFirstEducation: ${myFirstEducation}, type: ${typeof (myFirstEducation)}`);
+
+
+
+//Завдання 7
+
+let yourLogin = prompt ("Enter your Login: "); 
+console.log (`Your login is  ${yourLogin}!`);
+
+let yourEmail = prompt ("Enter your email: ");
+console.log (`Your email is  ${yourEmail}!`);
+
+let yourPassword = prompt ("Enter your password: ");
+console.log (`Your password is  ${yourPassword}!`);
+
+
+alert (`Dear  ${yourLogin} your email is  ${yourEmail} and your password is  ${yourPassword} !`);
+
